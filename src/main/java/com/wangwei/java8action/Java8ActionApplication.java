d30bbf3da65aec7a271145df562ad09a7e7e570e
wb_wangwei18 <wb_wangwei18@meituan.com>
2021-06-20 18:25:51 +0800

    项目初始化

---
+++ src/main/java/com/wangwei/java8action/Java8ActionApplication.java
@@ -0,0 +1,13 @@
+package com.wangwei.java8action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangwei/java8action/Java8ActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangwei.java8action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
